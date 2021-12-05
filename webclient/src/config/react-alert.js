ae5f8fff5982fa9178be4e2e2823f617990cd72b
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2021-12-06 05:23:03 +0800

    change react alert position

--- webclient/src/config/react-alert.js
+++ webclient/src/config/react-alert.js
@@ -7,7 +7,7 @@ import BasicAlertTemplate from 'react-alert-template-basic';
 const TIMEOUT_SECONDS = 5;
 // options for react alert
 export default {
-  position: positions.TOP_CENTER,
+  position: positions.BOTTOM_RIGHT,
   timeout: TIMEOUT_SECONDS*1000,
   transition: transitions.SCALE,
   template: BasicAlertTemplate
