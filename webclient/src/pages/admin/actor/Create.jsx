f61a7e39a2cfc90a2c1acb4dd7a1bc06adbccb1b
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2021-12-10 03:45:17 +0800

    actor img upload preview

--- webclient/src/pages/admin/actor/Create.jsx
+++ webclient/src/pages/admin/actor/Create.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Helmet} from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 import { Button, Container, FloatingLabel, Form } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import {create} from '../../../features/admin/actorSlice';
-
+import ImgFieldGallery from '../../../components/ImgFieldGallery';
 export default function Create() {
   const dispatch = useDispatch();
-  
+  const [imgUploads, setImgUploads] = useState([]);
   const submitHandler = (formEvent) => {
     formEvent.preventDefault();
     let formData = new FormData(formEvent.target);
@@ -44,7 +44,23 @@ export default function Create() {
               placeholder="Notes"
             />
           </FloatingLabel>
+          <Form.Group className="mb-4" controlId="createActorImages">
+            <Form.Label>Images</Form.Label>
+            <Form.Control 
+              type="file" 
+              multiple 
+              name="images" 
+              accept="image/*"
+              onChange={e=>{setImgUploads(e.target.files);}}
+            />
+          </Form.Group>
           <Button type="submit">Submit</Button>
+          
+          {imgUploads.length>0 && (<>
+            <h5>Upload Preview</h5>
+            <ImgFieldGallery fileList={imgUploads}/>
+          </>
+          )}
         </Form>
       </Container>
       
