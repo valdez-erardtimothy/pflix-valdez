6500f36651ddd06420fa67da488f04b6c1037e3c
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2022-01-03 11:40:29 +0800

    topMoviesReviewsChart

--- webclient/src/pages/admin/Dashboard.jsx
+++ webclient/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,18 @@
 import React, {useEffect} from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useDispatch, useSelector } from 'react-redux';
-import {Container, Row, Col} from 'react-bootstrap';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import {Doughnut} from 'react-chartjs-2';
+import {Container, Row, Col, Table} from 'react-bootstrap';
+import { Chart as ChartJS, 
+  ArcElement, 
+  Tooltip, 
+  Legend,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement
+} from 'chart.js';
+import {Doughnut, Chart} from 'react-chartjs-2';
 
 /* action import */
 import {load} from '../../features/admin/dashboardSlice';
@@ -12,7 +21,15 @@ import {startLoad, endLoad} from '../../features/loadingSlice';
 /* helper import */
 import {generateRGBAStrings} from '../../helpers/colorGenerator';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement,
+  Tooltip, 
+  Legend, 
+  LinearScale, 
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement
+);
 
 export default function Dashboard() {
   /* hooks  */
@@ -40,7 +57,7 @@ export default function Dashboard() {
     };
   }, []);
 
-  /* helper methods  */
+  /* chart object property generation methods */
 
   const showCountPieData = ({tvCount, movieCount}) => {
     return {
@@ -51,6 +68,43 @@ export default function Dashboard() {
         borderWidth:2,
         ...generateRGBAStrings(2)
       }]
+    };
+  };
+
+  const topMoviesChartObject = ({topMovies}) => {
+    return {
+      data: {
+        labels: topMovies.map(movie=>movie.title),
+        datasets:[
+          {
+            label: "Rating",
+            type:"bar",
+            yAxisID: "avgRating",
+            data: topMovies.map(movie=>movie.ratings),
+            ...generateRGBAStrings(1),
+          },
+          {
+            label: "Votes",
+            type:"line",
+            yAxisID: "votes",
+            data: topMovies.map(movie=>movie.reviewCount),
+            ...generateRGBAStrings(1),
+          },
+        ]
+      },
+      options: {
+        scales: {
+          avgRating: {
+            type:"linear",
+            position:"left"
+          },
+          votes: {
+            type: 'linear',
+            position: "right"
+          }
+        }
+      }
+
     };
   };
 
@@ -66,20 +120,62 @@ export default function Dashboard() {
       <title>Dashboard</title>
     </Helmet>
     <Container fluid>
-      <Row className="text-center" >
-        <Col sm="6" className="bg-light">
-          <h4>Shows</h4>
-          <Doughnut 
-            data={showCountPieData({tvCount,movieCount})}
-          />
-          <p className="mt-2">Total: {showCount}</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col md="6">
-          
-        </Col>
-      </Row>
+      {loadStatus === "success" && (
+        <> 
+          <Row className="text-center" >
+            <Col sm="5" className="bg-light">
+              <h4>Shows</h4>
+              <Doughnut 
+                data={showCountPieData({tvCount,movieCount})}
+              />
+              <p className="mt-2">Total: {showCount}</p>
+            </Col>
+            <Col sm={7}>
+              <Row>
+                <Col md="6">
+                  <h6>Top Rated Movies</h6>
+                  <Table size="sm" borderless striped hover className='text-start'>
+                    <thead>
+                      <tr>
+                        <th>Title</th>
+                        <th>Rating 
+                          <span className="text-muted">
+                       (reviews)
+                          </span>
+                        </th>
+                      </tr>
+
+                    </thead>
+                    <tbody>
+                      {topMovies.map(movie=>{
+                        return <tr key={movie._id}>
+                          <td>{movie.title}</td>
+                          <td>{movie.ratings.toFixed(2)} &nbsp;
+                            <small className="text-muted">
+                          ({movie.reviewCount})
+                            </small>
+                          </td>
+                        </tr>;
+                      })}
+                    </tbody>
+                  </Table>
+
+                </Col>
+              </Row>
+              <Row sm="6">
+                
+              </Row>
+            </Col>
+          </Row>
+          <div className="bg-light mt-4 p-1">
+            <h4 className='p-2'>Top rated Movies</h4>
+            <Chart {...topMoviesChartObject({topMovies})}/>
+
+          </div>
+        </>
+
+      )}
+     
     </Container>
   </>;
 }
