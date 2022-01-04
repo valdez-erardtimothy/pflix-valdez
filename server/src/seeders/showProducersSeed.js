6f360ac393990f266fe16c8abf0aaaffdac97597
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2022-01-04 18:18:09 +0800

    amend

--- server/src/seeders/showProducersSeed.js
+++ server/src/seeders/showProducersSeed.js
@@ -13,7 +13,7 @@ module.exports = {
     await Promise.all(producers.map(async producer => {
       await Promise.all(shows.map(async (show) => {
         // skip some shows to simulate "randomness"
-        if (!faker.datatype.boolean) return;
+        if (!faker.datatype.boolean()) return;
 
         await showProducerModel.create({
           producer: producer._id,
