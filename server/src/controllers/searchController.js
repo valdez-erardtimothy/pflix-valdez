e4c0f3efebdec89a4decedd8f78f733ee849c76c
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2022-01-08 00:03:08 +0800

    search controller try catch wrap

--- server/src/controllers/searchController.js
+++ server/src/controllers/searchController.js
@@ -4,43 +4,46 @@ let searchController = {};
 
 
 searchController.search = async (req, res, next) => {
-  // retrieve search params
-  let {
-    keyword,
-    entity = "show",
-    rating = "none",
-    dateStart,
-    dateEnd
-  } = req.query;
-  // model to query
-  let entityModel;
-  // query conditions setup
-  let where = {};
-  console.debug('rating:', rating)
-  if (rating !== "none") {
-    where.ratings = { $gte: parseInt(rating) };
-  }
-  keyword = new RegExp(keyword, 'i');
-  // set model and respective fields for keyword condition
-  if (entity == "actor") {
-    entityModel = actorModel;
-    where.name = keyword;
-
-  } else if (entity == "show") {
-    entityModel = showModel;
-    where.title = keyword;
-    if (dateStart && dateEnd) {
-      where.released = { $gte: dateStart, $lte: dateEnd }
+  try {
+    // retrieve search params
+    let {
+      keyword,
+      entity = "show",
+      rating = "none",
+      dateStart,
+      dateEnd
+    } = req.query;
+    // model to query
+    let entityModel;
+    // query conditions setup
+    let where = {};
+    if (rating !== "none") {
+      where.ratings = { $gte: parseInt(rating) };
     }
+    keyword = new RegExp(keyword, 'i');
+    // set model and respective fields for keyword condition
+    if (entity == "actor") {
+      entityModel = actorModel;
+      where.name = keyword;
+
+    } else if (entity == "show") {
+      entityModel = showModel;
+      where.title = keyword;
+      if (dateStart && dateEnd) {
+        where.released = { $gte: dateStart, $lte: dateEnd }
+      }
+    }
+    // do the query 
+    let data = await entityModel.find(where);
+
+
+    return res.status(200).send({
+      results: data,
+    });
+  } catch (e) {
+    return next(e)
   }
-  // do the query 
-  console.debug('entityModel =', entityModel)
-  let data = await entityModel.find(where);
 
-
-  res.status(200).send({
-    results: data,
-  });
 }
 
 
